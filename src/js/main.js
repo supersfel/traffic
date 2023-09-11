a5c498718455a5582f92eef85ddab8acd246a018
정민규 <jmg081005@naver.com>
2023-09-11 13:46:20 +0900

    feat: 드롭다운 애니메이션 추가

--- src/js/main.js
+++ src/js/main.js
@@ -16,14 +16,17 @@ const locationAnalyzeList = [
 // 영상분석 종류 선택
 function showTypeAnalyzeList() {
   const htmlEl = document.querySelector(".left .type form");
+  const typeEl = document.querySelector(".main__body .left .type p");
 
+  const selectType = typeAnalyzeList.filter((v) => v.isCheck);
+  typeEl.innerText = selectType.length
+    ? `${selectType[0].content} 영상현황`
+    : "종류별 영상분석";
   // 옵션 목록을 HTML 요소로 변환
   const htmlAry = typeAnalyzeList.map(
-    (el, idx) => `<div class="input-box">
+    (el, idx) => `<div class="input-box" onClick="clickTypeAnalyze(${idx})">
     ${el.content} 영상현황
-    <input type="checkbox" key=${idx} onClick="clickTypeAnalyze(${idx})" ${
-      el.isCheck ? "checked" : ""
-    } />
+    <input type="checkbox" key=${idx}  ${el.isCheck ? "checked" : ""} />
       </div>`
   );
   htmlEl.innerHTML = "";
@@ -46,14 +49,18 @@ function clickTypeAnalyze(idx) {
 // 지역종류 선택
 function showLocationAnalyzeList() {
   const htmlEl = document.querySelector(".left .location form");
+  const locationEl = document.querySelector(".main__body .left .location p");
+
+  const selectLocation = locationAnalyzeList.filter((v) => v.isCheck);
+  locationEl.innerText = selectLocation.length
+    ? `${selectLocation[0].content}`
+    : "지역별 영상분석";
 
   // 지역 옵션 목록을 HTML 요소로 변환
   const htmlAry = locationAnalyzeList.map(
-    (el, idx) => `<div class="input-box">
+    (el, idx) => `<div class="input-box" onClick="clickLocationAnalyze(${idx})">
       ${el.content}
-      <input type="checkbox" key=${idx}  onClick="clickLocationAnalyze(${idx})" ${
-      el.isCheck ? "checked" : ""
-    }/>
+      <input type="checkbox" key=${idx}   ${el.isCheck ? "checked" : ""}/>
         </div>`
   );
   htmlEl.innerHTML = "";
@@ -86,4 +93,33 @@ function moveAnalyzePage() {
   const type = typeAnalyzeList[typeIdx].content; // 선택한 영상분석 종류
   const location = locationAnalyzeList[locationIdx].content; // 선택한 지역
   window.location.href = `/src/html/analyze.html?type=${type}&location=${location}`; // 분석 페이지로 이동
+}
+
+/**
+ * 드롭다운 영역
+ */
+function clickTypeDropDown() {
+  const type = document.querySelector(".main__body .left .flex-box .type");
+
+  const classList = type.className.split(" ");
+
+  const newClassName = classList.includes("active")
+    ? "analyze type shadow-box"
+    : "analyze type shadow-box active";
+
+  type.className = newClassName;
+}
+
+function clickLocationDropDown() {
+  const location = document.querySelector(
+    ".main__body .left .flex-box .location"
+  );
+
+  const classList = location.className.split(" ");
+
+  const newClassName = classList.includes("active")
+    ? "analyze location shadow-box"
+    : "analyze location shadow-box active";
+
+  location.className = newClassName;
 }
